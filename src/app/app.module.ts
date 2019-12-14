bf7baac867c5f04ff4cce9181ce549a4a88a063f
4afS <4afS.h1@gmail.com>
2019-12-15 08:52:12 +0900

    finish chapter 3-5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-import { IncrementalSearchComponent } from './incremental-search/incremental-search.component';
-import { MessageComponent } from './message/message.component';
+import {AppComponent} from './app.component';
+import {HelloComponent} from './hello/hello.component';
+import {IncrementalSearchComponent} from './incremental-search/incremental-search.component';
+import {MessageComponent} from './message/message.component';
+import {MystyleDirective} from './mystyle.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HelloComponent,
     IncrementalSearchComponent,
-    MessageComponent
+    MessageComponent,
+    MystyleDirective
   ],
   imports: [
     BrowserModule,
@@ -22,4 +24,5 @@ import { MessageComponent } from './message/message.component';
   providers: [],
   bootstrap: [HelloComponent]
 })
-export class AppModule { }
+
+export class AppModule {}
